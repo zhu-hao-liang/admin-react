e132ac83767d5c2227350ad209840002cb0dcc05
zhu-hao-liang <1683331891@qq.com>
2019-07-02 14:40:13 +0800

    搭建基本结构

---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+import {Button} from 'antd'
+export default class Admin extends Component {
+    render() {
+        return (
+            <div>
+                <Button type='primary'>测试</Button>
+            </div>
+        )
+    }
+}
